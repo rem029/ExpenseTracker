033f50e22d9d29bf61edb2aa5011f8538811fc67
Lawrence Ponce <elawrenceponce@gmail.com>
2020-08-07 14:11:20 +0300

    Updates

---
+++ precache-manifest.5130fd297752eacec934e38d5729e37f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee0221ab284a2161ff9d43241e1f7af2",
+    "url": "/ExpenseTracker/index.html"
+  },
+  {
+    "revision": "3f68bbe8c26d65f3ff06",
+    "url": "/ExpenseTracker/static/css/main.c5981e02.chunk.css"
+  },
+  {
+    "revision": "380397396983161ec662",
+    "url": "/ExpenseTracker/static/js/2.9dea8c48.chunk.js"
+  },
+  {
+    "revision": "0fce063a4ec3cb005e7265693b407036",
+    "url": "/ExpenseTracker/static/js/2.9dea8c48.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3f68bbe8c26d65f3ff06",
+    "url": "/ExpenseTracker/static/js/main.1523eb76.chunk.js"
+  },
+  {
+    "revision": "a79201e28d91d48a8753",
+    "url": "/ExpenseTracker/static/js/runtime-main.8b5d7d42.js"
+  }
+]);
